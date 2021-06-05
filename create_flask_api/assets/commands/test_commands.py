35ce6d0468ccdbe5fd2d462d18ee45bb235135b9
Pedro Henrique Germano Silva <pedrohenriquegs2001@gmail.com>
2021-06-05 13:06:56 -0300

    feat: add windowns venv activate

--- create_flask_api/assets/commands/test_commands.py
+++ create_flask_api/assets/commands/test_commands.py
@@ -1,7 +1,7 @@
 def test_commands(project):
     return {
         'commands':[
-            f'cd {project.project_name} && source venv/bin/activate && pip install pytest && pip freeze > requirements.txt'
+            f'cd {project.project_name} && {project.venv_cmd} venv/bin/activate && pip install pytest && pip freeze > requirements.txt'
         ],
         'message': '\n\n --*-- Installing Pytest Packages --*-- \n\n' 
     }
--- create_flask_api/project.py
+++ create_flask_api/project.py
@@ -1,10 +1,13 @@
 from os.path import join
+import os, subprocess
 import re
 
 
 class ProjectSpecs:
+
     def __init__(self, project_path, args):
         self.project_name = args.project_name
         self.folder_name = re.sub("([\\\/:?<>|]*)$", "", args.project_name)
         self.project_path = join(project_path, self.folder_name)
         self.app_path = join(project_path, self.folder_name, "app")
+        self.venv_cmd = 'source' if not subprocess.call(["source"], stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT, shell=True) else "."
